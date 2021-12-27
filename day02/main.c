19501c64062d54fcd6cbfbfb38f3d64dfb1a3722
Julian Krämer <piscilus@gmail.com>
2021-12-27 16:38:59 +0100

    Optimizing code of day02

--- day02/main.c
+++ day02/main.c
@@ -1,24 +1,31 @@
 /**
  * \file main.c
  *
- * \copyright (C) 2021 piscilus
+ * \copyright (C) 2021 "piscilus" Julian Kraemer
  *
- * \brief Main program for advent of code 2021 day 1.
+ * Distributed under MIT license.
+ * See file LICENSE for details or copy at https://opensource.org/licenses/MIT
+ *
+ * \brief Main program for advent of code 2021 day 2.
  */
 
 /*---- Includes --------------------------------------------------------------*/
-#include "record_access.h"
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 
 /*---- Local macro definitions -----------------------------------------------*/
-#define SAMPLE_BUFFER_SIZE (2000)
+#define COMMAND_BUFFER_SIZE (2000)
+#define DIRECTION_MAX (10)
 
 
 /*---- Local type definitions ------------------------------------------------*/
+typedef struct command
+{
+    char d[DIRECTION_MAX];  /* direction */
+    unsigned int v;         /* velocity */
+} command_t;
 
 
 /*---- Local function prototypes ---------------------------------------------*/
@@ -41,37 +48,45 @@ unsigned int dive_part2(command_t commands[], int n);
 /*---- Exported functions ----------------------------------------------------*/
 int main ( int argc, char *argv[] )
 {
+    command_t commands[COMMAND_BUFFER_SIZE] = { 0 };
+    int num_cmds = 0;
     unsigned int result;
-    int n;
-    command_t commands[SAMPLE_BUFFER_SIZE] = {0};
 
     printf("Advent of Code 2021 - Day 2: Dive!\n\n");
 
-    if ( argc != 2)
+    if ( argc != 2 )
     {
-        printf("Please provide data record file name.");
-        return EXIT_FAILURE;
+        fprintf(stderr, "Please provide data record file name.");
+        exit(EXIT_FAILURE);
     }
 
-    n = read_data_record(argv[1], commands, SAMPLE_BUFFER_SIZE);
-    if ( n == 0 )
+    FILE* fp = fopen(argv[1], "r");
+
+    if ( !fp )
     {
-        printf("Reading data record failed");
-        return EXIT_FAILURE;
+        fprintf(stderr, "Could not open file!");
+        exit(EXIT_FAILURE);
     }
-    if ( n < 0)
+
+    while ( EOF != fscanf(fp, "%s %u\n", &commands[num_cmds].d[0], &commands[num_cmds].v) )
     {
-        printf("Buffer too small, more data available");
-        return EXIT_FAILURE;
+        if ( num_cmds >= COMMAND_BUFFER_SIZE )
+        {
+            fprintf(stderr, "Buffer overflow, too much data!");
+            exit(EXIT_FAILURE);
+        }
+        num_cmds++;
     }
+
+    fclose(fp);
 
     printf("Part 1:\n");
-    result = dive_part1(commands, n);
-    printf("result: %d\n\n", result);
+    result = dive_part1(commands, num_cmds);
+    printf("\tresult = %d\n\n", result);
 
     printf("Part 2:\n");
-    result = dive_part2(commands, n);
-    printf("result: %d\n", result);
+    result = dive_part2(commands, num_cmds);
+    printf("\tresult = %d\n", result);
 
     return EXIT_SUCCESS;
 }
@@ -86,17 +101,17 @@ unsigned int dive_part1(command_t commands[], int n)
         unsigned int d; /* depth */
     } position = {0U, 0U};
 
-    for (int i = 0; i < n; i++)
+    for ( int i = 0; i < n; i++ )
     {
-        if (strcmp("forward", commands[i].d) == 0)
+        if ( strcmp("forward", commands[i].d) == 0 )
         {
             position.h += commands[i].v;
         }
-        else if (strcmp("down", commands[i].d) == 0)
+        else if ( strcmp("down", commands[i].d) == 0 )
         {
             position.d += commands[i].v;
         }
-        else if (strcmp("up", commands[i].d) == 0)
+        else if ( strcmp("up", commands[i].d) == 0 )
         {
             position.d -= commands[i].v;
         }
@@ -120,18 +135,18 @@ unsigned int dive_part2(command_t commands[], int n)
         unsigned int a; /* aim */
     } position = {0U, 0U, 0U};
 
-    for (int i = 0; i < n; i++)
+    for ( int i = 0; i < n; i++ )
     {
-        if (strcmp("forward", commands[i].d) == 0)
+        if ( strcmp("forward", commands[i].d) == 0 )
         {
             position.h += commands[i].v;
             position.d += (position.a * commands[i].v);
         }
-        else if (strcmp("down", commands[i].d) == 0)
+        else if ( strcmp("down", commands[i].d) == 0 )
         {
             position.a += commands[i].v;
         }
-        else if (strcmp("up", commands[i].d) == 0)
+        else if ( strcmp("up", commands[i].d) == 0 )
         {
             if ( position.a < commands[i].v )
                 return EXIT_FAILURE;
